73144dc3e85437bc65658812281693a16fe06a63
kdmin0706 <gxxk76@gmail.com>
2024-05-20 07:33:13 +0900

    [feat]: initial setting

---
+++ src/main/java/sample/commutingsystem/CommutingSystemApplication.java
@@ -0,0 +1,13 @@
+package sample.commutingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommutingSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommutingSystemApplication.class, args);
+  }
+
+}
---
+++ src/test/java/sample/commutingsystem/CommutingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.commutingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommutingSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
