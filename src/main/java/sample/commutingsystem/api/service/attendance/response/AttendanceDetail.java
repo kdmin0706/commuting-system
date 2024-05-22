997b767d2b77d88e1ad9bc858c4b4c303ba1ea0b
kdmin0706 <gxxk76@gmail.com>
2024-05-23 08:04:14 +0900

    [refactor]: 특정기간 내에 직원의 근무 시간 조회 리팩토링  - 연차 사용 여부 boolean 변수 추가

--- src/main/java/sample/commutingsystem/api/service/attendance/response/AttendanceDetail.java
+++ src/main/java/sample/commutingsystem/api/service/attendance/response/AttendanceDetail.java
@@ -7,17 +7,20 @@ import lombok.Getter;
 public class AttendanceDetail {
   private String date;
   private int workingMinutes;
+  private boolean usingDayOff;
 
   @Builder
-  public AttendanceDetail(String date, int workingMinutes) {
+  public AttendanceDetail(String date, int workingMinutes, boolean usingDayOff) {
     this.date = date;
     this.workingMinutes = workingMinutes;
+    this.usingDayOff = usingDayOff;
   }
 
   public static AttendanceDetail create(String date, int workingMinutes) {
     return AttendanceDetail.builder()
         .date(date)
         .workingMinutes(workingMinutes)
+        .usingDayOff(workingMinutes == 0)
         .build();
   }
 
